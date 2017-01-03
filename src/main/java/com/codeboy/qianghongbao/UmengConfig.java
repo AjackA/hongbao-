55b31c01b55343d024b1283618880fa5f91c3f46
AjackA <hzg1310969428@163.com>
2017-01-03 12:51:13 +0800

    src

---
+++ src/main/java/com/codeboy/qianghongbao/UmengConfig.java
@@ -0,0 +1,15 @@
+package com.codeboy.qianghongbao;
+
+import android.content.Context;
+
+
+public final class UmengConfig {
+
+    private UmengConfig() {
+
+    }
+
+    public static boolean isEnableWechat(Context context) {
+        return true;
+    }
+}
